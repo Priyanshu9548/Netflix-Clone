8dcc1673b39d465e9fd3ef2a20b96f8e1ed0a52e
priyanshu9548 <priyanshusisodiya7088@gmail.com>
2023-03-28 13:02:33 +0530

    Movie List & Movie Card Components, Movies Endpoint, Cool hover effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,37 @@
 import Billboard from "@/components/Billboard";
+// import MovieList2 from "@/components/MovieList2";
+import MovieList from "@/components/MovieList";
+// import MovieList2 from "@/components/MovieList2";
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
-import { NextPageContext } from "next"
-import {getSession,signOut} from "next-auth/react"
-export async function getServerSideProps(context:NextPageContext){
-  const session =await getSession(context);
-  if(!session){
-    return{
-      redirect:{
-        destination:"/auth",
-        permanent :false,
-      }
-    }
+import useMovieList from "@/hooks/useMovieList";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
   }
-  return{
-    props:{}
-  }
+  return {
+    props: {},
+  };
 }
 export default function Home() {
-  const{ data:user}=useCurrentUser();
+  const { data: movies = [] } = useMovieList();
+
+  const { data: user } = useCurrentUser();
   return (
     <>
-     <Navbar/>  
-     <Billboard/>
-      </>
-  )
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
+    </>
+  );
 }
